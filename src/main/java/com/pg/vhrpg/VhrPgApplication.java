5ef1ca586ef57556635f0c86eb15b10e56771eed
潘高 <1473140658@qq.com>
2019-12-02 22:47:34 +0800

    搭建项目--首次提交

---
+++ src/main/java/com/pg/vhrpg/VhrPgApplication.java
@@ -0,0 +1,13 @@
+package com.pg.vhrpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrPgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrPgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pg/vhrpg/VhrPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg.vhrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhrPgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
